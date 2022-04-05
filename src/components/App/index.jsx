3ab3141482a487a3a9de63c03e13db24b20cf6cd
Pierre-Yves DELETTRE <dev.py@delettre.me>
2022-04-05 12:43:06 +0200

    create compnents directory
    add App component

---
+++ src/components/App/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const Example = () => (
+    <div class={styles.app}>
+        <h1>Hello quiz</h1>
+    </div>
+)
+
+export default Example;
